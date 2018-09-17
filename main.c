06a34069f3863a72ff5fafdfa42dbc4f7b0b4e0c
Frank Theile <frank.theile@web.de>
2018-09-17 18:10:41 +0200

    Same as before, but with structures for GPIO chip and GPIO line

--- main.c
+++ main.c
@@ -10,24 +10,36 @@
 //     struct client* me
 // - me-pointer in C == this-pointer in C++
 struct client {
-	int dummy;  // no members yet, but struct can't be empty...
+	struct gpiod_chip* chip;
+	struct gpiod_line* line;
 };
 
 // Static, local instance for now...:
 static struct client theApp;
 
-// Called after manipulating the GPIO,
-// before clean up by libgpiod
-static void client_wait(void* data)
-{
-	struct client* me = (struct client*)data;  // not used yet
-	usleep(5000*1000);
-}
-
 int main(int argc, char* argv[])
 {
 	printf("Version: %s\n", gpiod_version_string());
-	// Set line 4 on gpiochip0 'high' and call client_wait() afterwards: 
-	gpiod_ctxless_set_value("/dev/gpiochip0", 4, 1, false, argv[0], client_wait, &theApp);
+	theApp.chip = gpiod_chip_open("/dev/gpiochip0");
+	if (theApp.chip) {
+		theApp.line = gpiod_chip_get_line(theApp.chip, 4);
+		if (theApp.line) {
+			if (gpiod_line_request_output(theApp.line, argv[0], 0) == 0) {
+				if (gpiod_line_set_value(theApp.line, 1) == 0) {
+					usleep(5000*1000);
+				} else {
+					printf("gpiod_line_set_value() error\n");
+				}
+				gpiod_line_release(theApp.line);
+			} else {
+				printf("gpiod_line_request_output() error\n");
+			}
+		} else {
+			printf("gpiod_chip_get_line() error\n");
+		}
+		gpiod_chip_close(theApp.chip);
+	} else {
+		printf("gpiod_chip_open() error\n");
+	}
 	return 0;
 }
