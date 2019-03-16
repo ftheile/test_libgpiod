c6523453b77960edb80f9803bdec246dd255db06
Frank Theile <frank.theile@web.de>
2019-03-16 12:30:21 +0100

    Fix no. of flashes; check for flash count < 0

--- main.c
+++ main.c
@@ -38,51 +38,53 @@ int main(int argc, char* argv[])
 			theApp.line = gpiod_chip_get_line(theApp.chip, atoi(argv[2]));
 			if (theApp.line) {
 				int cnt = atoi(argv[3]);
-				if (!strcmp(argv[1], "-w") && (argc == 5)) {
-					// Write GPIO:
-					if (!gpiod_line_request_output(theApp.line, argv[0], 0)) {
-						int delay = 1000 * atoi(argv[4]);
-						int value = 1;
-						cnt *= 2;
-						do {
-							if (!gpiod_line_set_value(theApp.line, value)) {
-								usleep(delay);
-							} else {
-								perror("gpiod_line_set_value()");
-							}
-							value = !value;
-						} while (cnt--);
-						gpiod_line_release(theApp.line);
-					} else {
-						perror("gpiod_line_request_output()");
-					}
-				} else {
-					if (!strcmp(argv[1], "-r") && (argc == 4)) {
-						// Read GPIO:
-						if (!gpiod_line_request_falling_edge_events(theApp.line, argv[0])) {
+				if (cnt > 0) {
+					if (!strcmp(argv[1], "-w") && (argc == 5)) {
+						// Write GPIO:
+						if (!gpiod_line_request_output(theApp.line, argv[0], 0)) {
+							int delay = 1000 * atoi(argv[4]);
+							int value = 1;
+							cnt = 2 * cnt - 1;
 							do {
-								int r = gpiod_line_event_wait(theApp.line, NULL);
-								if (r == 1) {
-									struct gpiod_line_event evt;
-									if (!gpiod_line_event_read(theApp.line, &evt)) {
-										printf("Event!\n");
-									} else {
-										perror("gpiod_line_event_read()");
-									}
+								if (!gpiod_line_set_value(theApp.line, value)) {
+									usleep(delay);
 								} else {
-									if (r == 0) {
-										printf("Timeout\n");
-									} else {
-										perror("gpiod_line_event_wait()");
-									}
+									perror("gpiod_line_set_value()");
 								}
+								value = !value;
 							} while (cnt--);
 							gpiod_line_release(theApp.line);
 						} else {
-							perror("gpiod_line_request_falling_edge_events()");
+							perror("gpiod_line_request_output()");
 						}
 					} else {
-						Usage(argv[0]);
+						if (!strcmp(argv[1], "-r") && (argc == 4)) {
+							// Read GPIO:
+							if (!gpiod_line_request_falling_edge_events(theApp.line, argv[0])) {
+								do {
+									int r = gpiod_line_event_wait(theApp.line, NULL);
+									if (r == 1) {
+										struct gpiod_line_event evt;
+										if (!gpiod_line_event_read(theApp.line, &evt)) {
+											printf("Event!\n");
+										} else {
+											perror("gpiod_line_event_read()");
+										}
+									} else {
+										if (r == 0) {
+											printf("Timeout\n");
+										} else {
+											perror("gpiod_line_event_wait()");
+										}
+									}
+								} while (cnt--);
+								gpiod_line_release(theApp.line);
+							} else {
+								perror("gpiod_line_request_falling_edge_events()");
+							}
+						} else {
+							Usage(argv[0]);
+						}
 					}
 				}
 			} else {
